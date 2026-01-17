4e2eda9dcc9f9705dba9a544d9fa18a33e86406f
TroyPowrBomb-dad <troy.kindled@gmail.com>
2026-01-17 15:35:39 -0600

    added ability to change videos

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ const io = require('socket.io')(http);
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
-    console.log('A friend joined the party!');
+    console.log('A friend joined!');
 
-    // When someone hits play/pause, send it to everyone else
     socket.on('video_action', (data) => {
         socket.broadcast.emit('sync_action', data);
+    });
+
+    // ADD THIS PART:
+    socket.on('change_video', (data) => {
+        // This tells everyone to load the new Video ID
+        io.emit('sync_video', data); 
     });
 });
 
